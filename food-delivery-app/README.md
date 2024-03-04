22c8f4e1102c79725ee71fb9face76ea38c8f10b
Avinash Upadhyay <avinash.upadhyay@in.ey.com>
2024-03-04 15:33:24 +0530

    minor

--- food-delivery-app/README.md
+++ food-delivery-app/README.md
@@ -1,6 +1,6 @@
 # FoodDeliveryApp
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.1.0.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.0.3.
 
 ## Development server
 
--- food-delivery-app/src/app/app.component.spec.ts
+++ food-delivery-app/src/app/app.component.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
+    declarations: [AppComponent]
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'food-delivery-app' title`, () => {
+  it(`should have as title 'food-delivery-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('food-delivery-app');
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, food-delivery-app');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('food-delivery-app app is running!');
   });
 });
